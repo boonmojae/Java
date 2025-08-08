0f91319d57f54c274f2dc14d674d7786125d7f5c
boonmojae <pminny99@naver.com>
2025-08-08 18:42:12 +0900

    실습

---
+++ java-lecture/src/_02_control_statement/Practice1.java
@@ -0,0 +1,17 @@
+package _02_control_statement;
+
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("숫자를 입력하세요");
+        int number = scan.nextInt();
+
+        for (int i=1; i<=number; i++) {
+            System.out.print(i+ " ");
+        }
+        scan.close();
+    }
+}
