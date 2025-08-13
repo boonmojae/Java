3fe8996bbb91e1d0f283bb0cf94ef39f5c43aa5e
boonmojae <pminny99@naver.com>
2025-08-13 17:43:52 +0900

    Rectangle getter/setter Practice

--- java-lecture/src/_05_class/Rectangle.java
+++ java-lecture/src/_05_class/Rectangle.java
@@ -2,15 +2,30 @@ package _05_class;
 
 public class Rectangle {
 
-    int width;
-    int height;
+    private int width;
+    private int height;
 
-    public Rectangle(int width, int height) {
+    public Rectangle(int width) {
         this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
         this.height = height;
     }
 
-    public int area(int width, int height) {
+    public int area() {
         return width * height;
     }
 }
--- java-lecture/src/_05_class/RectangleEx.java
+++ java-lecture/src/_05_class/RectangleEx.java
@@ -1,17 +1,52 @@
 package _05_class;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class RectangleEx {
     public static void main(String[] args) {
 
+        ArrayList<Rectangle> list = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
-        System.out.println("사각형의 가로와 세로 길이를 띄어쓰기를 기준으로 입력해주세요.");
-        int w = scan.nextInt();
-        int h = scan.nextInt();
 
-        Rectangle r1 = new Rectangle(w, h);
+        try {
+            while (true) {
+                System.out.println("사각형의 가로와 세로 길이를 띄어쓰기를 기준으로 입력해주세요.");
+                int w = scan.nextInt();
+                int y = scan.nextInt();
 
-        System.out.println(r1.area(w,h));
+                if (w == 0 && y == 0) {
+                    break;
+                }
+
+                Rectangle rectangle = new Rectangle(w);
+                rectangle.setHeight(y);
+                list.add(rectangle);
+            }
+
+//            for (int i = 0; i < list.size(); i++) {
+//                Rectangle rt = list.get(i);
+//                System.out.println("가로 길이는: " + rt.getWidth());
+//                System.out.println("세로 길이는: " + rt.getHeight());
+//                System.out.println("넓이는: " + rt.area());
+//                System.out.println("--------------------");
+//            }
+
+            for (Rectangle element : list) {
+                System.out.println("가로 길이는: " + element.getWidth());
+                System.out.println("세로 길이는: " + element.getHeight());
+                System.out.println("넓이는: " + element.area());
+                System.out.println("--------------------");
+            }
+
+        } catch (IllegalArgumentException | NegativeArraySizeException e) {
+            System.out.println(e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("숫자를 입력해주세요.");
+            System.out.println(e.getMessage());
+        }
+        scan.close();
     }
 }
